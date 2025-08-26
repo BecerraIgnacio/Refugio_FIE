d737cffa00d1b4a77e6d7c1f92d38955b85ea9cf
Juan Ignacio Goñe <gjuanignacio@proton.me>
2025-08-26 09:56:54 -0300

    mascotas resueltas

--- mascota.py
+++ mascota.py
@@ -5,8 +5,11 @@ class Mascota:
         self.fecha_de_ingreso = fecha_de_ingreso
         self.id = id
 
+    def rehabilitado(self):
+        return (date.today() - self.fecha_de_ingreso).days >= self.rehabilitacion
+
     def saludar(self):
-        if(date.today() - self.fecha_de_ingreso).days >= self.rehabilitacion:
+        if self.rehabilitado():
             print(f"{self.apodo} dice {self.ruido}")
             return
         print(f"{self.apodo} no fue rehabilitado aun")
