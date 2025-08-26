f3cba224fec91fa90a4cff578d873d43de675160
Juan Ignacio Goñe <gjuanignacio@proton.me>
2025-08-26 11:48:32 -0300

    abm perros pajaros gatos en refugio

--- refugio.py
+++ refugio.py
@@ -20,3 +20,32 @@ class Refugio:
     def listar_adoptantes(self):
         for adoptante in self.lista_adoptante:
             print(adoptante)
+
+    def agregar_perro(self, perro):
+        self.lista_perros.add(perro)
+
+    def eliminar_perro(self, perro):
+        self.lista_perros.remove(perro)
+
+    def listar_perro(self):
+        for perro in self.lista_perros:
+            perro.saludar()
+
+    def agregar_gato(self, gato):
+        self.lista_gatos.add(gato)
+
+    def eliminar_gato(self, gato):
+        self.lista_gatos.remove(gato)
+
+    def listar_gato(self):
+        for gato in self.lista_gatos:
+            gato.saludar()
+    def agregar_pajaro(self, pajaro):
+        self.lista_pajaros.add(pajaro)
+
+    def eliminar_pajaro(self, pajaro):
+        self.lista_pajaros.remove(pajaro)
+
+    def listar_pajaro(self):
+        for pajaro in self.lista_pajaros:
+            pajaro.saludar()
